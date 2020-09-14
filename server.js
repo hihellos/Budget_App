32dca1b3590aee0d48bd7fe0d01a4060eafe6f47
hihello <13111181+hihellos@users.noreply.github.com>
2020-09-14 18:08:32 -0400

    updated server

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3000
 
 const app = express();
 
+app.use(logger("dev"));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,7 +21,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { // h
 });
 
 // routes
-app.use(require("./routes/api.js")); // middleware, directions for when the routes are hit, implies your routs files are using Router();
+app.use(require("./routes/api.js")); // middleware, directions for when the routes are hit, implies your route files are using Router();
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
